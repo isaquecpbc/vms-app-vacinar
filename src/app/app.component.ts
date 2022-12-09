c5bff7e57df2def1fb0edf10ff00901ba03d0fa4
Luis Silva <luissilva@brq.com>
2022-12-09 00:56:59 -0300

    nova conexão com banco

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { AfterViewChecked, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { AuthIntegrationService } from './services/auth-integration.service';
 import { DatabaseService } from './services/database.service';
+import { SQLiteService } from './services/sqlite.service';
 
 @Component({
   selector: 'app-root',
@@ -17,16 +18,21 @@ export class AppComponent implements OnInit {
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   public isLogged = false;
+  public initPlugin = false;
 
   constructor(
     private authService: AuthIntegrationService,
     private databaseService: DatabaseService,
+    private _sqlite: SQLiteService,
   ) {
     this.ouvirStatusLogin();
   }
 
   ngOnInit() {
-
+    this._sqlite.initializePlugin().then(ret => {
+      this.initPlugin = ret;
+      console.log('>>>> in App  this.initPlugin ' + this.initPlugin);
+    });
   }
   async initializeApp() {
       this.databaseService.init();
